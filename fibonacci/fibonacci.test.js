d6801efda5dba5375be6d80ef755f2e1490d989c
JCPedroza <nemesis4go10@hotmail.com>
2021-10-28 18:40:22 -0500

    use the fun property to call functions

--- fibonacci/fibonacci.test.js
+++ fibonacci/fibonacci.test.js
@@ -4,28 +4,28 @@ const fibRecursiveMemo = require('./fibonacci-recursive-memo')
 const fibRecursiveMinimal = require('./fibonacci-recursive-minimal')
 
 const checkFirstFiboNums = (fiboFunction) => {
-  const expected = [1, 1, 1, 1, 2, 3, 5, 8]
-  const inputs = [-1, 0, 1, 2, 3, 4, 5, 6]
+  const inputs = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const expectedOutputs = [1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
   const results = inputs.map(input => fiboFunction(input))
 
-  expect(results).toEqual(expected)
+  expect(results).toEqual(expectedOutputs)
 }
 
 const buildTestInfo = (functionId) =>
   `${functionId} fib(n) calculates the first fibonacci numbers`
 
 test(buildTestInfo('for loop'), () => {
-  checkFirstFiboNums(fibForLoop)
+  checkFirstFiboNums(fibForLoop.fun)
 })
 
 test(buildTestInfo('recursive'), () => {
-  checkFirstFiboNums(fibRecursive)
+  checkFirstFiboNums(fibRecursive.fun)
 })
 
 test(buildTestInfo('recursive with memoization'), () => {
-  checkFirstFiboNums(fibRecursiveMemo)
+  checkFirstFiboNums(fibRecursiveMemo.fun)
 })
 
 test(buildTestInfo('recursive minimalistic'), () => {
-  checkFirstFiboNums(fibRecursiveMinimal)
+  checkFirstFiboNums(fibRecursiveMinimal.fun)
 })
