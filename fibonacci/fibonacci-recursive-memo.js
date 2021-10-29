c4bdcaf64341fd175f3d338876f8c01f672f5e9f
JCPedroza <nemesis4go10@hotmail.com>
2021-10-28 19:28:25 -0500

    fix: recursive calls now pass the memo object as argument

--- fibonacci/fibonacci-recursive-memo.js
+++ fibonacci/fibonacci-recursive-memo.js
@@ -2,7 +2,7 @@ const fib = (n, memo = {}) => {
   if (n in memo) return memo[n]
   if (n < 3) return 1
 
-  memo[n] = fib(n - 1) + fib(n - 2)
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
   return memo[n]
 }
 
