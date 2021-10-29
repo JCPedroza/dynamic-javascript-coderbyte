736664a5a604c7969fc50eb1b8bb2e04b538a004
JCPedroza <nemesis4go10@hotmail.com>
2021-10-29 15:13:07 -0500

    refactor: use forEach to generalize test execution

--- fibonacci/fibonacci.test.js
+++ fibonacci/fibonacci.test.js
@@ -3,6 +3,13 @@ const fibRecursive = require('./fibonacci-recursive')
 const fibRecursiveMemo = require('./fibonacci-recursive-memo')
 const fibRecursiveMinimal = require('./fibonacci-recursive-minimal')
 
+const testSubjects = [
+  fibForLoop,
+  fibRecursive,
+  fibRecursiveMemo,
+  fibRecursiveMinimal
+]
+
 const checkFirstFiboNums = (fiboFunction) => {
   const inputs = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const expectedOutputs = [1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
@@ -14,18 +21,8 @@ const checkFirstFiboNums = (fiboFunction) => {
 const buildTestInfo = (functionId) =>
   `${functionId} fib(n) calculates the first fibonacci numbers`
 
-test(buildTestInfo('for loop'), () => {
-  checkFirstFiboNums(fibForLoop.fun)
+const runTest = (funInfo) => test(buildTestInfo(funInfo.id), () => {
+  checkFirstFiboNums(funInfo.fun)
 })
 
-test(buildTestInfo('recursive'), () => {
-  checkFirstFiboNums(fibRecursive.fun)
-})
-
-test(buildTestInfo('recursive with memoization'), () => {
-  checkFirstFiboNums(fibRecursiveMemo.fun)
-})
-
-test(buildTestInfo('recursive minimalistic'), () => {
-  checkFirstFiboNums(fibRecursiveMinimal.fun)
-})
+testSubjects.forEach(funInfo => runTest(funInfo))
