d5409301c7c1f79bb7f82a21eb537457a7af4722
JCPedroza <nemesis4go10@hotmail.com>
2021-10-29 21:48:09 -0500

    test, implement, document, profile

---
+++ fibonacci/fibonacci-tabulation.js
@@ -0,0 +1,26 @@
+/**
+ * Calculates the nth Fibonacci number.
+ * Uses the tabulation pattern.
+ * Time complexity: O(n)
+ * Memory complexity: O(n)
+ */
+
+const fib = (n) => {
+  if (n < 0) return undefined
+  n = Math.floor(n)
+
+  const fibNums = Array(n + 1).fill(0)
+  fibNums[1] = 1
+
+  for (let index = 0; index <= n; index++) {
+    fibNums[index + 1] += fibNums[index]
+    fibNums[index + 2] += fibNums[index]
+  }
+
+  return fibNums[n]
+}
+
+module.exports = {
+  fun: fib,
+  id: 'tabulation'
+}
