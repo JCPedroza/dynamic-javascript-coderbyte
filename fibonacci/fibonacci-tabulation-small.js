a34105db8fe2913bc40ff907deb85dc0b15d345b
JCPedroza <nemesis4go10@hotmail.com>
2021-10-29 20:16:06 -0500

    refactor: adjust return values for function domain & add module jsdoc

---
+++ fibonacci/fibonacci-tabulation-small.js
@@ -0,0 +1,25 @@
+/**
+ * Calculates the nth Fibonacci number.
+ * Uses the tabulation pattern.
+ * The array only contains the two last calculated values.
+ * Time complexity: O(n)
+ * Memory complexity: O(1)
+ */
+
+const fib = (n) => {
+  n = Math.floor(n)
+  if (n < 0) return undefined
+  if (n < 2) return n
+
+  const fibNums = [1, 1] // [last, current]
+  for (let round = 2; round < n; round++) {
+    [fibNums[0], fibNums[1]] = [fibNums[1], fibNums[0] + fibNums[1]]
+  }
+
+  return fibNums[1]
+}
+
+module.exports = {
+  fun: fib,
+  id: 'tabulation small array'
+}
